8efc62493ce6d0d5a4fba3067cf82d4799d6d138
shima <shima.jin@icloud.com>
2017-04-04 02:11:30 +0900

    Delete Storyboard

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        viewController.view.backgroundColor = UIColor.white
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
