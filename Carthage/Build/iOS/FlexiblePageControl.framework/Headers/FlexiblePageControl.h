29c0b843133e7d45019db088c83fa6513cb28220
shima <shima.jin@icloud.com>
2017-04-08 12:52:35 +0900

    Add Carthage

---
+++ Carthage/Build/iOS/FlexiblePageControl.framework/Headers/FlexiblePageControl.h
@@ -0,0 +1,19 @@
+//
+//  FlexiblePageControl.h
+//  FlexiblePageControl
+//
+//  Created by 島仁誠 on 2017/04/04.
+//  Copyright © 2017年 jinsei shima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlexiblePageControl.
+FOUNDATION_EXPORT double FlexiblePageControlVersionNumber;
+
+//! Project version string for FlexiblePageControl.
+FOUNDATION_EXPORT const unsigned char FlexiblePageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexiblePageControl/PublicHeader.h>
+
+
